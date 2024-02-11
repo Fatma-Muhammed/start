48f99e51ddcb4d68f609dc6a793e6d1abc28b5f9
fatma <fatma01006414502@gmail.com>
2024-02-11 21:00:32 +0200

    first commit

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+export class PortfolioComponent {
+
+
+  removeClass(member:HTMLDivElement):void{
+    member.classList.remove('d-none')
+
+  }
+  addClass(add:HTMLDivElement) : void{
+    add.classList.add('d-none')
+
+  }
+}
+
